92f128d13ded73c55dbe671ac2267bc3cb15fef3
GOD-GREEN <72262936+GOD-GREEN@users.noreply.github.com>
2021-11-03 21:26:38 -0300

    Add files via upload

--- EVOTPROJECT/JS/main.js
+++ EVOTPROJECT/JS/main.js
@@ -1,4 +1,29 @@
-function next(){
 
+function menuCadastra(){
+    
 }
+
+
+//############# CADASTRAR/LOGIN #####################//
+function Cadastrar(){
+    var CPF = document.getElementById('digiteSeuEMailOuCpf').value;
+    
+    
+    var dados = JSON.parse(localStorage.getItem("dadosUsuario"));
+
+    if(dados == null){
+        localStorage.setItem("dadosUsuario", "[]");
+        dados = [];
+    }
+    
+    var auxRegistro = {
+        nome: Usuario.value,
+        senha: Senha.value
+    }
+
+    dados.push(auxRegistro);
+
+    localStorage.setItem("dadosUsuario", JSON.stringify(dados));
+}
+
 
