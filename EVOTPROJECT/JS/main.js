56fbfa363df1f58aa6e2a1cdce4ab7a4130b09b5
GOD-GREEN <72262936+GOD-GREEN@users.noreply.github.com>
2021-11-03 22:09:34 -0300

    Add files via upload

--- EVOTPROJECT/JS/main.js
+++ EVOTPROJECT/JS/main.js
@@ -1,6 +1,34 @@
 
 function menuCadastra(){
+
+}
+
+function cadastraNota(){
+    var data  =  document.getElementById('dataNota').value
+    var COO   =   document.getElementById('COONota').value
+    var CNPJ  =  document.getElementById('CNPJNota').value
+    var valor = document.getElementById('valorNota').value
+
+    var nota = JSON.parse(localStorage.getItem("dadosNota"))
+
+    if(nota == null){
+        localStorage.setItem("dadosNota", "[]");
+        nota = [];
+    }
     
+    var auxRegistro = {
+        dataNota: data.value,
+        COONota: COO.value,
+        CNPJNota: CNPJ.value,
+        valorNota: valor.value
+    }
+
+    nota.push(auxRegistro);
+
+    localStorage.setItem("dadosNota", JSON.stringify(nota));
+    alert("Nota cadastrada com sucesso!")
+
+    document.form.reset();
 }
 
 
